fb0519581911e5c88f26a066c1a186c8e8b6da6c
Jelleas <jelle.van.assema@gmail.com>
2017-05-29 13:52:22 +0200

    exception cleanup

--- checkpy/exception.py
+++ checkpy/exception.py
@@ -16,19 +16,5 @@ class CheckpyError(Exception):
 class SourceException(CheckpyError):
 	pass
 
-
 class DownloadError(CheckpyError):
 	pass
-
-"""
-def TestException(Exception):
-	def __init__(self, exception, message):
-		self._exception = exception
-		self._message = message
-
-	def __str__(self):
-		return "An exception \"%s\" occured in the test itself" %str(self._exception) + " " + self._message
-
-	def __repr__(self):
-		return "TestException()"
-"""
