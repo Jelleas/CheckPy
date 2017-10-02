9835446474460f2f2518950b88e4ea2b97ee34df
jelleas <jelle.van.assema@gmail.com>
2017-10-02 13:48:02 +0200

    3.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
 	name='checkPy',
 
-	version='0.3.6',
+	version='0.3.7',
 
 	description='A simple python testing framework for educational purposes',
 	long_description=long_description,
