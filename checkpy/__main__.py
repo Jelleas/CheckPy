4b6a1f9fe7e31b12e4f6cf14ea0f5e42ce16014a
jelleas <jelle.van.assema@gmail.com>
2017-10-06 00:43:52 +0200

    added python version print in help

--- checkpy/__main__.py
+++ checkpy/__main__.py
@@ -7,7 +7,12 @@ import shutil
 import time
 
 def main():
-	parser = argparse.ArgumentParser(description="checkPy: a simple python testing framework")
+	parser = argparse.ArgumentParser(
+		description =
+			"checkPy: a python testing framework for education. You are running Python version {}.{}.{}."
+				.format(*sys.version_info[:3])
+	)
+
 	parser.add_argument("-module", action="store", dest="module", help="provide a module name or path to run all tests from the module, or target a module for a specific test")
 	parser.add_argument("-download", action="store", dest="githubLink", help="download tests from a Github repository and exit")
 	parser.add_argument("-update", action="store_true", help="update all downloaded tests and exit")
@@ -15,10 +20,10 @@ def main():
 	parser.add_argument("-clean", action="store_true", help="remove all tests from the tests folder and exit")
 	parser.add_argument("file", action="store", nargs="?", help="name of file to be tested")
 	args = parser.parse_args()
-	
+
 	rootPath = os.sep.join(os.path.abspath(os.path.dirname(__file__)).split(os.sep)[:-1])
 	if rootPath not in sys.path:
-		sys.path.append(rootPath)	
+		sys.path.append(rootPath)
 
 	if args.githubLink:
 		downloader.download(args.githubLink)
