2b03a85510a2dbe8f115281d6e3820107d235f40
jelleas <jelle.van.assema@gmail.com>
2019-01-22 13:43:34 +0100

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
 	name='checkPy',
 
-	version='0.4.5',
+	version='0.4.6',
 
 	description='A simple python testing framework for educational purposes',
 	long_description=long_description,
