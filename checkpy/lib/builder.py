71516bd3f3216dd02a99fd264311224427ea7d9c
Jelleas <jelle.van.assema@gmail.com>
2023-07-26 16:36:26 +0200

    FunctionBuilder docs

--- checkpy/lib/builder.py
+++ checkpy/lib/builder.py
@@ -14,17 +14,63 @@ __all__ = ["function", "FunctionBuilder", "FunctionState"]
 
 
 def function(functionName: str) -> "FunctionBuilder":
+    """
+    A declarative approach to writing checks through method chaining. For example:
+
+    testSquare = (
+        function("square")  # assert function square() is defined
+        .params("x")        # assert that square() accepts one parameter called x
+        .returnType("int")  # assert that the function always returns an integer
+        .call(2)            # call the function with argument 2
+        .returns(4)         # assert that the function returns 4
+        .call(3)            # now call the function with argument 3
+        .returns(9)         # assert that the function returns 9
+        .build()            # done, build the test
+    )
+
+    # Builders can be reused as long as build() is not called. For example:
+
+    squareBuilder = (
+        function("square")
+        .params("x")
+        .returnType("int")
+    )
+
+    testSquare2 = squareBuilder.call(2).returns(4).build() # do remember to call build()!
+    testSquare3 = squareBuilder.call(3).returns(9).build() # do remember to call build()!
+
+    # Tests created by this approach can depend and be depended on by other tests like normal:
+
+    testSquare4 = passed(testSquare2, testSquare3)(
+        squareBuilder.call(4).returns(16).build()  # testSquare4 will only run if both testSquare2 and 3 pass
+    )
+
+    @passed(testSquare2)
+    def testSquareError():  # testSquareError will only run if testSquare2 passes.
+        \"\"\"square("foo") raises a ValueError\"\"\"
+        try:
+            square("foo")
+        except ValueError:
+            return
+        return False
+    """
     return FunctionBuilder(functionName)
 
 
 class FunctionBuilder:
     def __init__(self, functionName: str):
+        """
+        A Builder of Function tests through method chaining.
+        Each method adds a part of a test on a stack.
+        Upon `.build()` a checkpy test is created that executes each entry in the stack. 
+        """
         self._state: FunctionState = FunctionState(functionName)
         self._blocks: List[Callable[["FunctionState"], None]] = []
 
         self.name(functionName)
 
     def name(self, functionName: str) -> "FunctionBuilder":
+        """Assert that a function with functionName is defined."""
         def testName(state: FunctionState):
             state.name = functionName
             state.description = f"defines the function {functionName}()"
@@ -35,6 +81,7 @@ class FunctionBuilder:
         return self
 
     def params(self, *params: str) -> "FunctionBuilder":
+        """Assert that the function accepts exactly these parameters."""
         def testParams(state: FunctionState):
             state.params = params
             state.description = f"defines the function as {state.name}({', '.join(params)})"
@@ -53,6 +100,12 @@ class FunctionBuilder:
         return self
 
     def returnType(self, type_: type) -> "FunctionBuilder":
+        """
+        Assert that the function always returns values of type_. 
+        Note that type_ can be any typehint. For instance:
+
+        function("square").returnType(Optional[int]) # assert that square returns an int or None
+        """
         def testType(state: FunctionState):
             state.returnType = type_
 
@@ -65,6 +118,7 @@ class FunctionBuilder:
         return self
 
     def returns(self, expected: Any) -> "FunctionBuilder":
+        """Assert that the last call returns expected."""
         def testReturned(state: FunctionState):
             actual = state.returned
             state.description = f"{state.getFunctionCallRepr()} should return {expected}"
@@ -73,22 +127,26 @@ class FunctionBuilder:
         self._blocks.append(testReturned)
         return self
     
-    def stdout(self, expected: Any) -> "FunctionBuilder":
+    def stdout(self, expected: str) -> "FunctionBuilder":
+        """Assert that the last call printed expected."""
         def testStdout(state: FunctionState):
             actual = state.function.printOutput
-            expectedStr = str(expected)
 
-            descrStr = expectedStr.replace("\n", "\\n")
+            descrStr = expected.replace("\n", "\\n")
             if len(descrStr) > 40:
                 descrStr = descrStr[:20] + " ... " + descrStr[-20:]
             state.description = f"{state.getFunctionCallRepr()} should print {descrStr}"
    
-            assert actual == expectedStr
+            assert actual == expected
 
         self._blocks.append(testStdout)
         return self
 
     def stdoutRegex(self, regex: Union[str, re.Pattern], readable: Optional[str]=None) -> "FunctionBuilder":
+        """
+        Assert that the last call printed output matching regex.
+        If readable is passed, show that instead of the regex in the test's output.
+        """
         def testStdoutRegex(state: FunctionState):
             nonlocal regex
             if isinstance(regex, str):
@@ -116,6 +174,7 @@ class FunctionBuilder:
         return self
 
     def call(self, *args: Any, **kwargs: Any) -> "FunctionBuilder":
+        """Call the function with args and kwargs."""
         def testCall(state: FunctionState):
             state.args = args
             state.kwargs = kwargs
@@ -131,6 +190,7 @@ class FunctionBuilder:
         return self
 
     def timeout(self, time: int) -> "FunctionBuilder":
+        """Reset the timeout on the check to time."""
         def setTimeout(state: FunctionState):
             state.timeout = time
 
@@ -138,17 +198,42 @@ class FunctionBuilder:
         return self
 
     def description(self, description: str) -> "FunctionBuilder":
+        """
+        Fixate the test's description on description. 
+        The test's description will not change after a call to this method,
+        and can only change by calling this method again.
+        """
         def setDecription(state: FunctionState):
+            state.setDescriptionFormatter(lambda descr, state: descr)
+            state.isDescriptionMutable = True
             state.description = description
+            state.isDescriptionMutable = False
 
         self._blocks.append(setDecription)
         return self
 
     def do(self, function: Callable[["FunctionState"], None]) -> "FunctionBuilder":
+        """
+        Put function on the internal stack and call it after all previous calls have resolved.
+        .do serves as an entry point for extensibility. Allowing you, the test writer, to insert
+        specific and custom asserts, hints, and the like. For example:
+
+        def checkDataFileIsUnchanged(state: "FunctionState"):
+            with open("data.txt") as f:
+                assert f.read() == "42\\n", "make sure not to change the file data.txt"
+        
+        test = function("process_data").call("data.txt").do(checkDataFileIsUnchanged).build()
+        """
         self._blocks.append(function)
         return self
 
     def build(self) -> checkpy.tests.TestFunction:
+        """
+        Build the actual test (checkpy.tests.TestFunction). This should always be the last call.
+        Be sure to store the result in a global, to allow checkpy to discover the test. For instance:
+
+        testSquare = (function("square").call(3).returns(9).build())
+        """
         def testFunction():
             self.log: List[FunctionState] = []
 
@@ -172,6 +257,7 @@ class FunctionState:
         self._args: List[Any] = []
         self._kwargs: Dict[str, Any] = {}
         self._timeout: int = 10
+        self._isDescriptionMutable: bool = True
         self._descriptionFormatter: Callable[[str, FunctionState], str] =\
             lambda descr, state: f"testing {state.name}() >> {descr}"
 
@@ -255,8 +341,18 @@ class FunctionState:
 
     @description.setter
     def description(self, newDescription):
+        if not self.isDescriptionMutable:
+            return
         self._description = newDescription
         checkpy.tester.getActiveTest().description = self.description
+
+    @property
+    def isDescriptionMutable(self):
+        return self._isDescriptionMutable
+
+    @isDescriptionMutable.setter
+    def isDescriptionMutable(self, newIsDescriptionMutable: bool):
+        self._isDescriptionMutable = newIsDescriptionMutable
 
     def getFunctionCallRepr(self):
         argsRepr = ", ".join(str(arg) for arg in self.args)
